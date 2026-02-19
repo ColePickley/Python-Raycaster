3ef8843a04973096566f524a10a6e68f58a8a23b
Cole Pickley <colepickley@gmail.com>
2026-02-19 17:59:03 -0600

    Delete Ray.py

--- Ray.py
+++
@@ -1,5 +0,0 @@
-class Ray:
-    def __init__(self, degree):
-        self.length = 0
-        self.degree = degree #is the degree from the players directional vector that the ray is cast from
-        self.is_vertical_collision = False
